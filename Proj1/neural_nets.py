9fba5aaa5a3e77f16c4206f640373b5820edc5e1
Charles Dufour <charles.dufour@epfl.ch>
2019-04-14 18:37:34 +0200

    change: try to construct a siamese neural network

--- Proj1/neural_nets.py
+++ Proj1/neural_nets.py
@@ -1,61 +1,45 @@
+'''
+implementation of a neural net that is trained to recognize the number and then do the comparison
+Use weight sharing , siamese type neural net
+'''
+
+################################################################################
+
 import torch
+import math
+
 from torch.autograd import Variable
 from torch import nn
 from torch import optim
-
 from torch.nn import functional as F
 
 import dlc_practical_prologue as prologue
 
+################################################################################
 
-class model_1(nn.Module):
+class Siamese_net(nn.Module):
+    '''network that will be shared by the two "channels" of the bigger network '''
     def __init__(self):
-        super(model_1, self).__init__()
-        self.conv1 = nn.Conv2d(2, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.fc1 = nn.Linear(64, 40)
-        self.fc2 = nn.Linear(40, 2)
+        super(Siamese_net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=5)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=2)
+        self.fc1 = nn.Linear(9 * 64, 200)
+        self.fc2 = nn.Linear(200, 10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=3, stride=3))
-        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2, stride=2))
-        x = F.relu(self.fc1(x.view(-1, 64)))
+        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=2))
+        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.fc1(x.view(-1, 9 * 64)))
         x = self.fc2(x)
         return x
 
-    def predict(self,x):
-        return torch.argmax(self(x),dim = 1)
 
-
-def comppute_nb_errors(predicted,true):
-    nb_errors = 0
-    for true,tried in zip(train_target,predicted):
-        if true.item() != tried.item():
-            nb_errors += 1
-
-    return nb_errors
-
-def train_model(model, train_input, train_target,test_input,test_target):
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr = 1e-1)
-    nb_epochs = 20
-    mini_batch_size = int(train_input.size(0)/200)
-
-    for e in range(nb_epochs):
-        for b in range(0, train_input.size(0), mini_batch_size):
-            output = model(train_input.narrow(0, b, mini_batch_size))
-            loss = criterion(output, train_target.narrow(0, b, mini_batch_size))
-            model.zero_grad()
-            loss.backward()
-            optimizer.step()
-        print("epoch {}, loss {:.3},  precision on test set {:2%}".format(e,loss, comppute_nb_errors(model.predict(test_input),test_target)/test_target.size(0)))
 
 if __name__ == "__main__":
 
-    #loading the data
-    train_input, train_target, train_classes, test_input, test_target, test_classes= prologue.generate_pair_sets(1000)
-    net = model_1()
-    out = net(train_input)
-    predicted = torch.argmax(out, dim = 1)
-
-    train_model(net,train_input,train_target,test_input,test_target)
+    #load the data
+    train_input, train_target, train_classes, test_input, test_target, test_classes = prologue.generate_pair_sets(1000)
+    model = Siamese_net()
+    print(model(train_input[0,0,:]).shape)
