08d50abc7a096bcbe5a6d5f3d7008b91329d0d72
Charles Dufour <charles.dufour@epfl.ch>
2019-04-18 11:30:35 +0200

    cleared, only new module

--- Proj1/neural_nets.py
+++ Proj1/neural_nets.py
@@ -22,6 +22,7 @@ import dlc_practical_prologue as prologue
 
 
 class SimpleNet(nn.Module):
+    ''' basic branch of the siamese net'''
 
     def __init__(self):
         super(SimpleNet,self).__init__()
@@ -42,10 +43,20 @@ class SimpleNet(nn.Module):
         _, predicted_class = torch.max(self.forward(x),1)
         return predicted_class
 
-class SiameseNet_test(nn.Module):
+class SiameseNet(nn.Module):
+    '''Siamese net module '''
 
     def __init__(self, branch = None):
-        super(SiameseNet_test, self).__init__()
+        """ constructor for the siamese net, branch should be a module of input size [n,1,14,14] and ouput [n,10]
+
+        Parameters
+        ----------
+        branch : nn.Module
+            branch of the siamese net (net that is shared) (the default is None).
+        >>>
+
+        """
+        super(SiameseNet, self).__init__()
         if branch == None:
             self.branch= SimpleNet()
         else:
@@ -62,6 +73,29 @@ class SiameseNet_test(nn.Module):
         return x
 
     def train(self,train_input, train_target, train_classes = None, auxilary = False, verbose = True, nb_epochs = 50):
+        """ Training of the siamese module.
+            if not auxilary:
+                usual training
+            if auxilary:
+                first split the sample into the two images and use the branch to try to classify the numbers, compute the
+                loss wrt to the classes of digits and update the parameters  of the branch.
+                Then run the entire sample trough the sample and compute the loss against the train_target and update
+
+        Parameters
+        ----------
+        train_input : [n,2,14,14]
+            two images [14,14] representing two numbers
+        train_target : [n,2]
+            is the first digit bigger than the second
+        train_classes : [n,2]
+            values of the two digits
+        auxilary : bool
+            use of the auxilary loss (the default is False).
+        verbose : bool
+            if True print the training  `verbose` (the default is True).
+        nb_epochs : int
+            epochs to train (the default is 50).
+        """
 
         if auxilary:
             print("training with auxilary loss with {} epochs".format(nb_epochs))
@@ -75,7 +109,7 @@ class SiameseNet_test(nn.Module):
         for e in range(nb_epochs):
 
             if auxilary:
-
+                #first pass
                 #separating the data so that we train on the two images separatly, and learn to classify them properly
                 train_input1,train_classes1,train_input2,train_classes2 = split_channels(train_input, train_classes)
 
@@ -91,7 +125,7 @@ class SiameseNet_test(nn.Module):
                 loss_aux.backward(retain_graph=True)
                 optimizer.step()
 
-
+                #second pass
                 #loss and optimization of the whole model
                 response = self.forward(train_input)
                 loss = criterion(response,train_target)
@@ -112,39 +146,6 @@ class SiameseNet_test(nn.Module):
 
 
 
-
-class SiameseNet(nn.Module):
-
-    def __init__(self):
-        super(SiameseNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.fc1 = nn.Linear(4*64 , 100)
-        self.fc2 = nn.Linear(100, 10)
-        #to combine the ouput into a prediction at the output of the SiameseNet
-        self.pooling = nn.Linear(20,2)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=2))
-        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2))
-        x = F.relu(self.fc1(x.view(-1, 4* 64)))
-        x = self.fc2(x)
-        return x
-
-    def predict(self,x):
-        ''' method to predict from the network with original output if the first digit is bigger than the second '''
-        input1 = torch.reshape(x[:,0,:], (x.shape[0],1,14,14))
-        input2 = torch.reshape(x[:,1,:], (x.shape[0],1,14,14))
-        out1 = self.forward(input1)
-        out2 = self.forward(input2)
-
-        #combine the two tensor on top o feach other
-        out = torch.cat((out2, out1), 1)
-        #perform actual prediction
-        _, response = torch.max(self.pooling(out),1)
-
-        return response
-
 def split_channels(input, classes):
     '''Separate the two images and corresponding classes '''
     #separating the data so that we train on the two images separatly, and learn to classify them properly
@@ -156,104 +157,11 @@ def split_channels(input, classes):
 
     return input1,classes1, input2,classes2
 
-def train_siamese(model,train_input,train_target, train_classes, nb_epochs = 25, verbose = True, aux = True):
-    """    train the siamese network based on the input being two images of 14x14
-    Parameters
-    ----------
-    model : SiameseNet
-        model to train
-    train_input : torch.tensor  [n,2,14,14]
-        input of pairs of images shape
-    train_target : torch.tensor [n]
-        class to predict (1 if digit of first image is bigger than second one)
-    train_classes : torch.tensor [n,2]
-        classes of the actual digits on the two images
-    nb_epochs : int
-        (the default is 25).
-    verbose : bool
-        (the default is True).
-    aux : bool
-        If true train the model with the auxilary loss (the default is True).
-
-    Examples
-    -------
-    >>> train_input, train_target, train_classes,\
-        test_input, test_target, test_classes = prologue.generate_pair_sets(1000)
-    >>> model = SiameseNet()
-    >>> train_siamese(model,train_input,train_target, train_classes, nb_epochs = 75, verbose = False)
-
-    """
-    #loss and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(),lr = 0.005)
-
-    #separating the data so that we train on the two images separatly, and learn to classify them properly
-    train_input1,train_classes1,train_input2,train_classes2 = split_channels(train_input, train_classes)
-
-    for e in range(nb_epochs):
-        out1 = model(train_input1)
-        out2 = model(train_input2)
-
-
-        if aux:
-            #auxilary loss: learn to detect the handwritten digits directly
-            loss_aux = criterion(out1,train_classes1)+criterion(out2,train_classes2)
-            model.zero_grad()
-            loss_aux.backward(retain_graph=True)
-            optimizer.step()
-
-
-        #combine the two tensor on top o feach other
-        out = torch.cat((out2, out1), 1)
-        #perform actual prediction
-        response = model.pooling(out)
-
-        #loss and optimization
-        loss = criterion(response,train_target)
-        model.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if verbose:
-            acc = old_accuracy(model, train_input, train_target)
-            print("epoch {:3}, loss {:7.4}, accuracy {:.2%}".format(e,loss,acc))
-
-def train_model(model, train_input, train_target, mini_batch_size, verbose = False, lr = 0.005, nb_epochs = 25):
-    ''' Simple on training for handwritten recognition'''
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(),lr = lr)
-
-    for e in range(nb_epochs):
-        for b in range(0, train_input.size(0), mini_batch_size):
-            output = model(train_input.narrow(0, b, mini_batch_size))
-            inter_target = train_target.narrow(0, b, mini_batch_size)
-            loss = criterion(output, train_target.narrow(0, b, mini_batch_size) )
-            model.zero_grad()
-            loss.backward()
-            optimizer.step()
-        if verbose:
-            print("Epoch {:3} loss {:7.4}".format(e, loss.data.numpy()))
 
 def accuracy(model,input,target):
-
+    
     #perform actual class prediction
     response = model(input).argmax(1)
-
-
-    error = 0
-
-    #compute the percentage of error
-    for tried,true in zip(response,target):
-        if tried != true: error+=1
-
-    return 1-error/response.shape[0]
-
-def old_accuracy(model,input,target):
-    #perform actual class prediction
-    response = model.predict(input)
-
-
     error = 0
 
     #compute the percentage of error
--- Proj1/test.py
+++ Proj1/test.py
@@ -10,7 +10,7 @@ from torch import optim
 from torch.nn import functional as F
 
 from dlc_practical_prologue import generate_pair_sets
-from neural_nets import accuracy,SiameseNet, train_siamese, SiameseNet_test, train_model,SimpleNet, old_accuracy
+from neural_nets import accuracy,SiameseNet, SimpleNet
 
 ################################################################################
 
@@ -23,36 +23,20 @@ train_input, train_classes = Variable(train_input), Variable(train_classes)
 
 epochs = 75
 
-print("--------------- with new module ---------------------------------")
 branch = SimpleNet()
-model = SiameseNet_test(branch = branch)
+model = SiameseNet(branch = branch)
 model.train(train_input, train_target, train_classes = train_classes, auxilary = True, verbose = True, nb_epochs = epochs)
-acc_train_new_with = accuracy(model,train_input,train_target)
-acc_test_new_with = accuracy(model,test_input,test_target)
+acc_train_with = accuracy(model,train_input,train_target)
+acc_test_with = accuracy(model,test_input,test_target)
 
 
 branch = SimpleNet()
-model = SiameseNet_test(branch = branch)
+model = SiameseNet(branch = branch)
 model.train(train_input, train_target, train_classes = train_classes, auxilary = False, verbose = True, nb_epochs = epochs)
-acc_train_new_withouth = accuracy(model,train_input,train_target)
-acc_test_new_withouth = accuracy(model,test_input,test_target)
+acc_train_withouth = accuracy(model,train_input,train_target)
+acc_test_withouth = accuracy(model,test_input,test_target)
 print("\n")
-print("------------------------ with old version -----------------------------")
 
-## testing with auxilary loss function (default setting)
-model = SiameseNet()
-print("training with auxilary loss function")
-train_siamese(model,train_input,train_target, train_classes, nb_epochs = epochs, verbose = True)
-acc_train_old_with = old_accuracy(model,train_input,train_target)
-acc_test_old_with = old_accuracy(model,test_input,test_target)
-## testing withouth auxilary loss
-model = SiameseNet()
-print("training with no auxilary loss function")
-train_siamese(model,train_input,train_target, train_classes, nb_epochs = epochs, verbose = True, aux = False)
-acc_train_old_withouth = old_accuracy(model,train_input,train_target)
-acc_test_old_withouth = old_accuracy(model,test_input,test_target)
 
-print("New module, avec:   accuracy on train {:4.2%} and on test {:4.2%}".format(acc_train_new_with,acc_test_new_with))
-print("New module, sans:   accuracy on train {:4.2%} and on test {:4.2%}".format(acc_train_new_withouth,acc_test_new_withouth))
-print("Old module, avec:   accuracy on train {:4.2%} and on test {:4.2%}".format(acc_train_old_with,acc_test_old_with))
-print("Old module, sans:   accuracy on train {:4.2%} and on test {:4.2%}".format(acc_train_old_withouth,acc_test_old_withouth))
+print("New module, auxilray:    accuracy on train {:4.2%} and on test {:4.2%}".format(acc_train_with,acc_test_with))
+print("New module, no auxilary: accuracy on train {:4.2%} and on test {:4.2%}".format(acc_train_withouth,acc_test_withouth))
