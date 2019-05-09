fixed derivative transmitted by the functionnals in the backward propagation

Proj2/Criterion.py
@@ -10,40 +10,51 @@ Need for an abstract class criterion ? seems to be a double of Module then
 
 class MSE(Module):
 
-    def forward(self,input, target):
-        input = input.view(target.size())
-        return ((input-target)**2).mean()
+    def __init__(self):
+        super(MSE,self).__init__()
+        self.input = torch.empty(1)
+        self.target = torch.empty(1)
 
-    def backward(self,input, target):
+    def forward(self,output, target):
+
+        #resize to avoid automatic casting
+        output = output.view(target.size())
+        self.input = output
+        self.target = target
+        return ((output-target)**2).mean()
+
+    def backward(self):
         '''
         return the derivative of the MSE loss with respect to the target
-
         '''
-        input = input.view(target.shape)
-        derivative = 2*(input-target)/input.numel()
+        derivative = 2*(self.input-self.target)/self.input.numel()
         return derivative
 
 class CrossEntropy(Module):
 
-    def forward(self, input, target):
-        raise NotImplementedError
+    def __init__(self):
+        super(CrossEntropy,self).__init__()
+        self.input = torch.empty(1)
+        self.target = torch.empty(1)
 
-    def backward(self, input, target):
-        raise NotImplementedError
+    def forward(self, output, target):
+
+        #stabilization trick to avoid Nan:
+        #     this is equivalent to multiply and divide by -max(output) in the ratio below
+        output_stabilized = output- torch.max(output)
+        self.input = output_stabilized
+        self.target = target
+
+        #compute normalzed probabilities based on multi-class logistic classification
+        proba = torch.exp(output_stabilized[range(target.shape[0]),target])/torch.exp(output_stabilized).sum(1)
+        return -torch.log(proba).mean()
+
+    def backward(self):
+
+        inter = self.input.softmax(1)
+        inter[range(self.target.shape[0]),self.target.long()] -= 1
+        return inter/self.input.shape[0]
 
 
-
-#Not sure about this one, just in case of, should be moved
-class Precision(Module):
-    '''
-    to compute precision in percentage: #right/#total * 100
-    '''
-    def forward(self, input, target):
-        error = 0
-        for tried,true in zip(input,target):
-            if tried != true: error+=1
-
-        return (1-error/input.shape[0])*100
-
-    def backward(self, input, target):
-        raise NotImplementedError
+    def __repr__(self):
+        print("Cross Entropy Loss")

Proj2/Functionnals.py
@@ -7,17 +7,28 @@ from Module import Module
 
 class Relu(Module):
 
+    def __init__(self):
+        super(Relu,self).__init__()
+        self.input = torch.empty(1)
+
+
     def forward(self,x):
         #apply Relu
         #return torch.max(x, torch.zeros_like(x))
+        self.input = x
         return torch.relu(x)
 
-    def backward(self,x):
-        #apply derivative of relu
-        inter = torch.max(x, torch.zeros_like(x))
+    def backward(self,dx):
+        #apply derivative of relu in the backward propagation scheme
+        # return the pointwise product of its derivative and the derivative of the next layer
+
+        #compute the derivative of the relu
+        inter = torch.max(self.input, torch.zeros_like(self.input))
         inter[inter <= 0.] = 0.
         inter[inter > 0.] = 1.
-        return inter
+
+        #return hadamard product
+        return inter*dx
 
 
     def __str__(self):
@@ -25,13 +36,18 @@ class Relu(Module):
 
 class Tanh(Module):
 
+    def __init__(self):
+        super(Tanh,self).__init__()
+        self.input = torch.empty(1)
+
     def forward(self,x):
-        #apply tanh
+        self.input = x
         return x.tanh()
 
-    def backward(self,x):
+    def backward(self,dx):
         #apply derivative of tanh
-        return 4*(x.exp() + x.mul(-1).exp()).pow(-2)
+        ds =  4*(self.input.exp() + self.input.mul(-1).exp()).pow(-2)
+        return ds*dx
 
     def __str__(self):
         return "tanh"

Proj2/Linear.py
@@ -11,7 +11,7 @@ class Linear(Module):
 
     def __init__(self, input_size, output_size):
         super(Linear,self).__init__()
-        self.weights = Parameters(torch.empty(output_size,input_size,dtype=torch.float32).normal_())
+        self.weights = Parameters(torch.empty(output_size,input_size,dtype=torch.float32).uniform_())
         self.bias = Parameters(torch.zeros(output_size,dtype=torch.float32))
         self.result = Parameters(torch.empty(output_size,dtype=torch.float32))
         self.input = Parameters(torch.empty(input_size,dtype=torch.float32))
@@ -70,7 +70,7 @@ class Linear(Module):
 
         #add a new dimension if necessary so that the derivative is a matrix not a vector
         if len(next_derivative.shape) == 1:
-            next_derivative = next_derivative.view(next_derivative.shape[0],1)
+            next_derivative = next_derivative.view(1,next_derivative.shape[0])
         if len(self.input.value.shape) == 1:
             x_i = self.input.value.view(1,self.input.value.shape[0])
         else:
@@ -83,6 +83,10 @@ class Linear(Module):
 
         # derivative with respect to the weights: dloss/dw
         self.weights.grad += torch.mm(next_derivative.t(),x_i)
+        # if torch.max(self.weights.grad).item() ==0:
+        #     print("INDICATION OF VANISHING GRADIENT")
+        #     print(self)
+        #     print(torch.max(self.input.value),torch.max(next_derivative))
 
         #derivative with respect to the bias: dloss/dbias
         self.bias.grad += torch.sum(next_derivative.t(), dim=1)

Proj2/test.py
@@ -39,7 +39,8 @@ def train_model(model, train_input, train_target):
             inter = criterion.backward(output,train_target.narrow(0, b, mini_batch_size))
             model.backward(inter)
             optimizer.step()
-        print("loss at epoch {:4}: {:10.6}, difference with previous {:6.4}".format(e,loss, abs(loss-old_loss)))
+        print("loss at epoch {:4}: {:10.6}, difference with previous {:6.4}".
+                format(e,loss, abs(loss-old_loss)))
 def train_model_per_data_point(model, train_input, train_target):
     criterion = Criterion.MSE()
     optimizer = SGD(model.param(), lr = 1e-1)
@@ -57,10 +58,35 @@ def train_model_per_data_point(model, train_input, train_target):
             model.backward(inter)
             optimizer.step()
         print("loss at epoch {}: {:4}".format(e,loss))
-def generate_disc_set(nb):
+def generate_disc_set(nb, loss = "cross"):
     input = torch.Tensor(nb, 2).uniform_(-1, 1)
-    target = input.pow(2).sum(1).sub(2 / math.pi).sign().add(1).div(2)
+    if loss == "mse":
+        target = input.pow(2).sum(1).sub(2 / math.pi).sign().add(1).div(2)
+    else:
+        target = input.pow(2).sum(1).sub(2 / math.pi).sign().add(1).div(2).long()
     return input, target
+def accuracy(model,input,target):
+
+    #perform actual class prediction
+    response = model.forward(input).argmax(1)
+    error = 0
+
+    #compute the percentage of error
+    for tried,true in zip(response,target):
+        if tried.item() != true.item(): error+=1
+
+    return 1-error/response.shape[0]
+def accuracy_ref(model,input,target):
+
+    #perform actual class prediction
+    response = model.forward(input).argmax(1)
+    error = 0
+
+    #compute the percentage of error
+    for tried,true in zip(response,target):
+        if tried.item() != true.item(): error+=1
+
+    return 1-error/response.shape[0]
 
 ############################################################################################
 
@@ -68,6 +94,20 @@ def generate_disc_set(nb):
 #        but not in the all data set
 
 which_test = "sequential"
+
+x = torch.tensor([[-1.,-3.,4.],[-3.,3.,-1.]])
+y = torch.tensor([0,1])
+
+print(x.softmax(1))
+print((x-torch.max(x)).softmax(1))
+
+print("mhh")
+print(x[range(y.shape[0]),y])
+
+test = Criterion.CrossEntropy()
+print(test.forward(x,y))
+
+
 
 
 #############################################################################################
@@ -100,40 +140,46 @@ if which_test == "training":
 ##############################################################################################
 if which_test == "sequential":
 
-
+    out = 1
 
 
     #creation of model from pytorch module, with same initialization
     class test_sequential(nn.Module):
         def __init__(self):
             super(test_sequential, self).__init__()
-            self.Fc1 = nn.Linear(2, 124)
-            self.Fc2 = nn.Linear(124,45)
-            self.Fc3 = nn.Linear(45,1)
+            self.Fc1 = nn.Linear(2, 2)
+            #self.Fc2 = nn.Linear(300,100)
+            #self.Fc3 = nn.Linear(100,45)
+            self.Fc4 = nn.Linear(2,out)
 
         def forward(self, x):
             x = F.relu(self.Fc1(x))
-            x = F.relu(self.Fc2(x))
-            x = self.Fc3(x)
-            return x
+            #x = F.relu(self.Fc2(x))
+            #x = F.relu(self.Fc3(x))
+            return self.Fc4(x)
 
 
     model = test_sequential()
 
 
     #creation of model from our module to test with the same initialization as the reference model
-    fc1 = Linear(2,124)
-    fc2 = Linear(124,45)
-    fc3 = Linear(45,1)
+    fc1 = Linear(2,2)
+    #fc2 = Linear(300,100)
+    #fc3 = Linear(100,45)
+    fc4 = Linear(2,out)
     fc1.weights.value = model.Fc1.weight.data.clone()
-    fc2.weights.value = model.Fc2.weight.data.clone()
-    fc3.weights.value = model.Fc3.weight.data.clone()
+    # fc2.weights.value = model.Fc2.weight.data.clone()
+    # fc3.weights.value = model.Fc3.weight.data.clone()
+    fc4.weights.value = model.Fc4.weight.data.clone()
     fc1.bias.value = model.Fc1.bias.data.clone()
-    fc2.bias.value = model.Fc2.bias.data.clone()
-    fc3.bias.value = model.Fc3.bias.data.clone()
+    # fc2.bias.value = model.Fc2.bias.data.clone()
+    # fc3.bias.value = model.Fc3.bias.data.clone()
+    fc4.bias.value = model.Fc4.bias.data.clone()
     sigma1 = Functionnals.Relu()
     sigma2 = Functionnals.Relu()
-    test = Sequential(fc1,sigma1,fc2,sigma2,fc3)
+    sigma3 = Functionnals.Relu()
+    #test = Sequential(fc1,sigma1,fc2,sigma2,fc3,sigma3,fc4)
+    test = Sequential(fc1,sigma1,fc4)
 
     print(model)
     print(test)
@@ -141,11 +187,12 @@ if which_test == "sequential":
 
 
     #creation of dummy data_set and target
-    X,target = generate_disc_set(1000)
+    X,target = generate_disc_set(1000,"mse")
 
     #normalization
-    mean, std = X.mean(), X.std()
-    X.sub_(mean).div_(std)
+    #mean, std = X.mean(), X.std()
+    #X.sub_(mean).div_(std)
+
 
 
 
@@ -154,95 +201,100 @@ if which_test == "sequential":
     criterion_ref = nn.MSELoss()
 
     #creation of the optimizers for the two models
-    lr = 0.1
+    lr = 0.9
     optim_test = SGD(test.param(), lr = lr)
     optim_ref = torch.optim.SGD(model.parameters(), lr = lr)
 
 
     #actual training in parallel with comparison of data along the way to monitor the differences
-    nb_epochs = 100
+    nb_epochs = 200
     mini_batch_size = 100
 
 
-    #plots_evolution
-    plot_ref = []
-    plot_test = []
-
 
     #check if initialization is the same for both models
-    print("-----------------------------------difference between the initial values")
-    print("weights")
-    print(torch.max(model.Fc1.weight- fc1.weights.value))
-    print(torch.max(model.Fc2.weight- fc2.weights.value))
-    print(torch.max(model.Fc3.weight- fc3.weights.value))
-    print("biases")
-    print(torch.max(model.Fc1.bias- fc1.bias.value))
-    print(torch.max(model.Fc2.bias- fc2.bias.value))
-    print(torch.max(model.Fc3.bias- fc3.bias.value))
+    # print("-----------------------------------difference between the initial values")
+    # print("weights")
+    # print(torch.max(model.Fc1.weight- fc1.weights.value))
+    # print(torch.max(model.Fc2.weight- fc2.weights.value))
+    # print(torch.max(model.Fc3.weight- fc3.weights.value))
+    # print("biases")
+    # print(torch.max(model.Fc1.bias- fc1.bias.value))
+    # print(torch.max(model.Fc2.bias- fc2.bias.value))
+    # print(torch.max(model.Fc3.bias- fc3.bias.value))
 
     for e in range(nb_epochs):
         loss_epoch_test = 0
         loss_epoch_ref = 0
-        print("-----------------------------------------------------------------  epoch {}".format(e))
+        #print("-----------------------------------------------------------------  epoch {}".format(e))
         for b in range(0, X.size(0), mini_batch_size):
 
-            input_narrow = X.narrow(0, b, mini_batch_size)
-
-            #my framework
-            output_test = test.forward(input_narrow)
-            loss_test = criterion_test.forward(output_test, target.narrow(0, b, mini_batch_size))
-            plot_test.append(loss_test)
-            loss_epoch_test += loss_test
-            test.zero_grad()
-            inter_test = criterion_test.backward(output_test,target.narrow(0, b, mini_batch_size))
-            test.backward(inter_test)
-            optim_test.step()
-
+            input_narrow_test = X.narrow(0, b, mini_batch_size)
+            input_narrow_ref = input_narrow_test.clone()
 
             #the reference framework
-            output_ref = model(input_narrow)
+            output_ref = model(input_narrow_ref)
+            # if e>1:
+            #     print(torch.max(old_ref-output_ref).item())
+            old_ref = output_ref
             loss_ref = criterion_ref(output_ref, target.narrow(0, b, mini_batch_size))
-            plot_ref.append(loss_ref)
             loss_epoch_ref += loss_ref
             model.zero_grad()
             loss_ref.backward()
             optim_ref.step()
 
 
+            #my framework
+            output_test = test.forward(input_narrow_test)
+            # if e>1:
+            #     print(torch.max(output_test-old).item())
+            old = output_test
+            #print(torch.cat([output_ref.t(),output_test.t()]))
+            loss_test = criterion_test.forward(output_test, target.narrow(0, b, mini_batch_size))
+            #print(loss_test)
+            loss_epoch_test += loss_test
+            test.zero_grad()
+            inter_test = criterion_test.backward(output_test,target.narrow(0, b, mini_batch_size))
+            test.backward(inter_test)
+
+            # for param in test.param():
+            #     print(torch.max(param.grad))
+            #     param.value -= lr*param.grad
+            optim_test.step()
+
+        print("epoch {:4}, loss_ref {:9.9}, loss_test {:9.9}".
+                format(e,loss_epoch_ref,loss_epoch_test))
+
+        predicted_test = accuracy(test,X,target)
+        predicted_ref = accuracy_ref(model,X,target)
+        print("precision ref {:.4%}, precision test {:.4%}".format(predicted_ref,predicted_test))
 
 
 
 
-        print("")
-        print("----------------------------------- global evolution")
-        print("My framework loss at epoch {:4}: {:10.6}".format(e,loss_epoch_test))
-        print("The ref      loss at epoch {:4}: {:10.6}".format(e,loss_epoch_ref))
-        print("----------------------------------- comparison of output")
-        print(torch.max(test.forward(X[0:200,:])-model(X[0:200,:])))
-
-        print("-----------------------------------differences between the loss")
-        print(torch.max(loss_epoch_test-loss_epoch_ref))
-        print("-----------------------------------difference between the gradients")
-        print("weights")
-        print(torch.max(model.Fc1.weight.grad- fc1.weights.grad))
-        print(torch.max(model.Fc2.weight.grad- fc2.weights.grad))
-        print(torch.max(model.Fc3.weight.grad- fc3.weights.grad))
-        print("biases")
-        print(torch.max(model.Fc1.bias.grad- fc1.bias.grad))
-        print(torch.max(model.Fc2.bias.grad- fc2.bias.grad))
-        print(torch.max(model.Fc3.bias.grad- fc3.bias.grad))
 
 
-    import matplotlib.pyplot as plt
-    import numpy as np
-    plt.plot(plot_test, label = "test", marker = ".")
-    plt.plot(plot_ref, label = "ref", marker = ".")
-    plt.legend()
-    plt.show()
-    plt.plot(np.array(plot_test)-np.array(plot_ref), label = "diff", marker = ".")
-    plt.plot(np.array(plot_test)/np.array(plot_ref), label = "ratio", marker = ".")
-    plt.legend()
-    plt.show()
+        # print("")
+        # print("----------------------------------- global evolution")
+        # print("My framework loss at epoch {:4}: {:10.6}".format(e,loss_epoch_test))
+        # print("The ref      loss at epoch {:4}: {:10.6}".format(e,loss_epoch_ref))
+        # print("----------------------------------- comparison of output")
+        # print(torch.max(test.forward(X[0:200,:])-model(X[0:200,:])))
+        #
+        # print("-----------------------------------differences between the loss")
+        # print(torch.max(loss_epoch_test-loss_epoch_ref))
+        # print("-----------------------------------difference between the gradients")
+        # print("weights")
+        # print(torch.max(model.Fc1.weight.grad- fc1.weights.grad))
+        # print(torch.max(model.Fc2.weight.grad- fc2.weights.grad))
+        # print(torch.max(model.Fc3.weight.grad- fc3.weights.grad))
+        # print("biases")
+        # print(torch.max(model.Fc1.bias.grad- fc1.bias.grad))
+        # print(torch.max(model.Fc2.bias.grad- fc2.bias.grad))
+        # print(torch.max(model.Fc3.bias.grad- fc3.bias.grad))
+
+
+
 
 ###############################################################################################
 if which_test == "gradient linear":

Proj2/test_all_parts.py
@@ -12,6 +12,7 @@ from Linear import Linear
 from Sequential import Sequential
 from Optimizer import SGD
 from Functionnals import Relu
+from Functionnals import Tanh
 
 ################################################################################
 number_tests = 1000
@@ -19,10 +20,9 @@ number_tests = 1000
 torch.set_default_dtype(torch.double)
 ################################################################################
 
-
 'test for MSE function'
-
-if True:
+if False:
+    print('test for MSE function')
     MSE_test = Criterion.MSE()
     MSE_ref = nn.MSELoss()
 
@@ -30,6 +30,7 @@ if True:
     n2 = 5
 
     mean_error = 0
+    mean_grad_diff = 0
 
     for i in range(number_tests):
         x_test = torch.empty(n1,n2).normal_(10)
@@ -38,17 +39,16 @@ if True:
         x_ref = x_test.clone()
         y_ref = y_test.clone()
         x_ref.requires_grad = True
-        y_ref.requires_grad = True
 
-        loss_ref = MSE_test.forward(x_ref,y_ref)
-        loss_test = MSE_ref(x_test,y_test)
+        loss_ref = MSE_ref(x_ref,y_ref)
+        loss_test = MSE_test.forward(x_test,y_test)
 
         diff_loss = torch.max(loss_ref-loss_test)
 
         if diff_loss > torch.tensor([0.]):
             mean_error += diff_loss
 
-        derivative_test = MSE_test.backward(x_ref,y_ref)
+        derivative_test = MSE_test.backward(x_test,y_test)
         x_ref.grad = None
         loss_ref.backward()
         derivative_ref = x_ref.grad
@@ -56,9 +56,212 @@ if True:
         diff_grad = torch.max(derivative_test-derivative_ref)
 
         if diff_grad > torch.tensor([0.]):
-            print("diff in gradient")
-            print(diff_grad)
-    print("mean error for losses {:.15%}".format(mean_error/number_tests))
+            mean_grad_diff += diff_grad
+    print("mean error for losses {:.15}".format(mean_error/number_tests))
+    print("mean error for gradient {:.15}".format(mean_grad_diff/number_tests))
+
 
 'evidence of computation issue below 1e-13'
+
 ################################################################################
+
+'test for Cross entropy loss'
+if False:
+    print('test for Cross entropy loss')
+    MSE_test = Criterion.CrossEntropy()
+    MSE_ref = nn.CrossEntropyLoss()
+
+    n1 = 10
+    n2 = 5
+
+    mean_error = 0
+    mean_grad_diff = 0
+
+    for i in range(number_tests):
+        x_test = torch.empty(n1,n2).normal_(10)
+        y_test = torch.LongTensor(10).random_(0, 5)
+
+        x_ref = x_test.clone()
+        y_ref = y_test.clone()
+        x_ref.requires_grad = True
+
+        loss_ref = MSE_ref(x_ref,y_ref)
+        loss_test = MSE_test.forward(x_test,y_test)
+
+        diff_loss = torch.max(loss_ref-loss_test)
+
+        if diff_loss > torch.tensor([0.]):
+            mean_error += diff_loss
+
+        derivative_test = MSE_test.backward(x_test,y_test)
+        x_ref.grad = None
+        loss_ref.backward()
+        derivative_ref = x_ref.grad
+
+        diff_grad = torch.max(derivative_test-derivative_ref)
+
+        if diff_grad > torch.tensor([0.]):
+            mean_grad_diff += diff_grad
+    print("mean error for losses   {:.15}".format(mean_error/number_tests))
+    print("mean error for gradient {:.15}".format(mean_grad_diff/number_tests))
+
+
+'evidence of difference of the order of 1e-16 and 1e-17'
+
+
+################################################################################
+'test for Linear'
+if False:
+    print('test for Linear')
+
+    n1 = 10
+    n2 = 5
+
+    mean_error = 0
+    mean_error_loss = 0
+    mean_grad_w_diff = 0
+    mean_grad_b_diff = 0
+
+
+    criterion_test = Criterion.MSE()
+    criterion_ref = nn.MSELoss()
+
+    for i in range(number_tests):
+
+
+        test = Linear(n1,n2)
+        ref = nn.Linear(n1,n2)
+
+        test.weights.value = ref.weight.data.clone()
+        test.bias.value = ref.bias.data.clone()
+
+        #building the summy datasets
+        x_test = torch.empty(n1).normal_(10)
+        y_test = torch.empty(n2).random_(0, 5)
+        x_ref = x_test.clone()
+        y_ref = y_test.clone()
+        x_ref.requires_grad = True
+
+        #comparing output
+        output_ref = ref(x_ref)
+        output_test = test.forward(x_test)
+        mean_error += torch.max(output_test-output_ref)
+
+
+        #comparing the losses again (in case of)
+        loss_test = criterion_test.forward(output_test,y_test)
+        loss_ref = criterion_ref(output_ref,y_ref)
+        mean_error_loss += torch.max(loss_test-loss_ref)
+
+        #comparing the gradient
+        test.zero_grad()
+        ref.zero_grad()
+        loss_ref.backward()
+        inter = criterion_test.backward(output_test,y_test)
+        test.backward(inter)
+
+        grad_w_ref = ref.weight.grad
+        grad_b_ref = ref.bias.grad
+        grad_w_test = test.weights.grad
+        grad_b_test = test.bias.grad
+
+        mean_grad_w_diff += torch.max(grad_w_test-grad_w_ref)
+        mean_grad_b_diff += torch.max(grad_b_test - grad_b_ref)
+
+    print("mean error for output   {}".format(mean_error/number_tests))
+    print("mean error for loss     {}".format(mean_error_loss/number_tests))
+    print("mean error for grad w   {}".format(mean_grad_w_diff/number_tests))
+    print("mean error for grad b   {}".format(mean_grad_b_diff/number_tests))
+
+################################################################################
+'test for Linear'
+if True:
+    print('test for Sequential')
+
+    n1 = 10
+    n2 = 5
+
+    mean_error = 0
+    mean_error_loss = 0
+    mean_grad_w_diff1 = 0
+    mean_grad_b_diff1 = 0
+    mean_grad_w_diff2 = 0
+    mean_grad_b_diff2 = 0
+
+
+    criterion_test = Criterion.MSE()
+    criterion_ref = nn.MSELoss()
+
+
+    class Model(nn.Module):
+        def __init__(self):
+            super(Model, self).__init__()
+            self.Fc1 = nn.Linear(n1, 100)
+            self.Fc2 = nn.Linear(100, n2)
+
+        def forward(self, x):
+            x = torch.tanh(self.Fc1(x))
+            x = self.Fc2(x)
+            return x
+
+    for i in range(number_tests):
+
+
+        test = Sequential(Linear(n1,100),Tanh(),Linear(100,n2))
+
+        ref = Model()
+
+        layer1_test = test.layers[0]
+        layer2_test = test.layers[-1]
+        layer1_test.weights.value = ref.Fc1.weight.data.clone()
+        layer1_test.bias.value = ref.Fc1.bias.data.clone()
+        layer2_test.weights.value = ref.Fc2.weight.data.clone()
+        layer2_test.bias.value = ref.Fc2.bias.data.clone()
+
+        #building the summy datasets
+        x_test = torch.empty(n1).normal_(10)
+        y_test = torch.empty(n2).random_(0, 5)
+        x_ref = x_test.clone()
+        y_ref = y_test.clone()
+        x_ref.requires_grad = True
+
+        #comparing output
+        output_ref = ref(x_ref)
+        output_test = test.forward(x_test)
+        mean_error += torch.max(output_test-output_ref)
+
+
+        #comparing the losses again (in case of)
+        loss_test = criterion_test.forward(output_test,y_test)
+        loss_ref = criterion_ref(output_ref,y_ref)
+        mean_error_loss += torch.max(loss_test-loss_ref)
+
+        #comparing the gradient
+        test.zero_grad()
+        ref.zero_grad()
+        loss_ref.backward()
+        inter = criterion_test.backward()
+        test.backward(inter)
+
+        grad_w_ref_layer1 = ref.Fc1.weight.grad
+        grad_b_ref_layer1 = ref.Fc1.bias.grad
+        grad_w_test_layer1 = layer1_test.weights.grad
+        grad_b_test_layer1 = layer1_test.bias.grad
+
+        grad_w_ref_layer2 = ref.Fc2.weight.grad
+        grad_b_ref_layer2 = ref.Fc2.bias.grad
+        grad_w_test_layer2 = layer2_test.weights.grad
+        grad_b_test_layer2 = layer2_test.bias.grad
+
+        mean_grad_w_diff1 += torch.max(grad_w_test_layer1-grad_w_ref_layer1)
+        mean_grad_b_diff1 += torch.max(grad_b_test_layer1 - grad_b_ref_layer1)
+        mean_grad_w_diff2 += torch.max(grad_w_test_layer1-grad_w_ref_layer1)
+        mean_grad_b_diff2 += torch.max(grad_b_test_layer1 - grad_b_ref_layer1)
+
+
+    print("mean error for output   {}".format(mean_error/number_tests))
+    print("mean error for loss     {}".format(mean_error_loss/number_tests))
+    print("mean error for grad w 1 {}".format(mean_grad_w_diff1/number_tests))
+    print("mean error for grad b 1 {}".format(mean_grad_b_diff1/number_tests))
+    print("mean error for grad w 2 {}".format(mean_grad_w_diff2/number_tests))
+    print("mean error for grad b 2 {}".format(mean_grad_b_diff2/number_tests))